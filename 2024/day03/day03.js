3a27afe37b09309a5df11fcc610f4da5d8582919
Konrad Romanowski <konrad.romanowski.it@gmail.com>
2024-12-03 18:33:01 +0100

    Added day03 part2 solution, renamed variables

--- 2024/day03/day03.js
+++ 2024/day03/day03.js
@@ -5,17 +5,31 @@ fs.readFile('input.txt','utf-8',(err,inputData) => {
     if(err) return console.log(err);
 
     const mulRegEx = /mul\(\d{1,3}\,\d{1,3}\)/g;
-    
-    let data = inputData.match(mulRegEx);
+    const data = inputData.match(mulRegEx);
 
     const factorRegEx = /\d{1,3}/g
-    const data1 = data.map(el => {
+    const multPairs1 = data.map(el => {
         return el.match(factorRegEx).map(n => parseInt(n));
     });
 
-    let part1 = data1.reduce((sum,pair) => {
+    let part1 = multPairs1.reduce((sum,pair) => {
         return sum += pair[0] * pair[1];
     },0);
 
     console.log(part1);
+
+    const regExDontDo = /don't\(\).*?do\(\)/g;
+
+    const simplifiedInputData = inputData.replace(/\r\n/g,"").replace(regExDontDo,"");
+    const simplifiedData = simplifiedInputData.match(mulRegEx);
+
+    const multPairs2 = simplifiedData.map(el => {
+        return el.match(factorRegEx).map(n => parseInt(n));
+    });
+
+    let part2 = multPairs2.reduce((sum,pair) => {
+        return sum += pair[0] * pair[1];
+    },0);
+
+    console.log(part2);
 });
