f97647d98c484ab1ad76b16ef29782b654828071
Konrad Romanowski <konrad.romanowski.it@gmail.com>
2024-12-12 18:50:12 +0100

    Updated day11 solution - now works for part2

--- 2024/day11/day11.js
+++ 2024/day11/day11.js
@@ -5,76 +5,25 @@ fs.readFile('input.txt','utf-8',(err,inputData) => {
     if(err) return console.log(err);
     
     let stones = inputData.split(" ");
-    let numOfBlinks = 25;
+    let numOfBlinks = 75;
 
-    // Simple solution for part1
-    // for(let n = 0; n < numOfBlinks; n++) {
-    //     for(let i = 0; i < stones.length; i++) {
-    //         if(stones[i] === '0') {
-    //             stones[i] = "1";
-    //         } else if(stones[i].length%2 ===0) {
-    //             let mid = stones[i].length/2;
-    //             let left = parseInt(stones[i].slice(0,mid)).toString();
-    //             let right = parseInt(stones[i].slice(mid)).toString();
-    //             stones.splice(i,1,left,right);
-    //             i++;
-    //         } else {
-    //             let newStone = parseInt(stones[i]) * 2024;
-    //             stones[i] = newStone.toString();
-    //         }
-    //     }
-    // }
-    // console.log(stones.length);
+    let part1 = 0;
+    let part2 = 0;
 
-    // Slightly better solution using memoization
-    // still not good enough for part2
-
-    // stones = inputData.split(" ");
-    // const memo = {
-    //     '0': ['1']
-    // }
-    
-    // for(let n = 0; n < numOfBlinks; n++) {
-    //     for(let i = 0; i < stones.length; i++) {
-    //         let stone = stones[i];
-    //         if(memo[stone]) {
-    //             stones.splice(i,1,...memo[stone]);
-    //             i+= memo[stone].length-1;
-    //         } else {
-    //             if(stone.length%2 === 0) {
-    //                 let mid = stone.length/2;
-    //                 let left = parseInt(stone.slice(0,mid)).toString();
-    //                 let right = parseInt(stone.slice(mid)).toString();
-    //                 memo[stone] = [left,right];
-    //             } else {
-    //                 let newStone = parseInt(stone) * 2024;
-    //                 memo[stone] = [newStone.toString()];
-    //             }
-    //             stones.splice(i,1,...memo[stone]);
-    //             i+= memo[stone].length-1;
-    //         }
-    //     }
-    // }
-
-    // Another improved solution
-    // This time not inserting calculated numbers 
-    // into the middle of array but pushing to new array
-    // and then overwriting the current stones array 
-    // still not good enough to solve part2
-
-    stones = inputData.split(" ");
     const memo = {
         '0': ['1']
     }
 
-    for(let n = 0; n < numOfBlinks; n++) {
-        let newStones = [];
-        for(let i = 0; i < stones.length; i++) {
-            let stone = stones[i];
-            if(memo[stone]) {
-                newStones.push(...memo[stone]);
-            } else {
-                if(stone.length%2 === 0) {
+    let stonesCount = {};
+    for(let stone of stones) {
+        stonesCount[stone] = (stonesCount[stone] || 0) + 1;
+    }
+
+    for(let n = 1; n <= numOfBlinks; n++) {
+        let newStonesCount = {};
+        for(let stone in stonesCount) {
+            if(!memo[stone]) {
+                if(stone.length % 2 === 0) {
                     let mid = stone.length/2;
                     let left = parseInt(stone.slice(0,mid)).toString();
                     let right = parseInt(stone.slice(mid)).toString();
@@ -83,12 +32,23 @@ fs.readFile('input.txt','utf-8',(err,inputData) => {
                     let newStone = parseInt(stone) * 2024;
                     memo[stone] = [newStone.toString()];
                 }
-                newStones.push(...memo[stone]);
+            }
+            memo[stone].forEach(s => {
+                newStonesCount[s] = (newStonesCount[s] || 0) + (stonesCount[stone] || 1);
+            });
+        }
+        stonesCount = newStonesCount
+        if(n === 25) {
+            for(const v of Object.values(stonesCount)) {
+                part1 += v;
             }
         }
-        stones = structuredClone(newStones);
     }
 
-    let part1 = stones.length;
     console.log(part1);
+
+    for(const v of Object.values(stonesCount)) {
+        part2 += v;
+    }
+    console.log(part2);
 });
