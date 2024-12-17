c37deb280ee459ed0dda1943d19982c5e551774d
Konrad Romanowski <konrad.romanowski.it@gmail.com>
2024-12-17 17:00:48 +0100

    Added day15 part2 solution

--- 2024/day15/day15.js
+++ 2024/day15/day15.js
@@ -9,6 +9,8 @@ fs.readFile('input.txt','utf-8',(err,inputData) => {
     const map = data[0].split("\r\n").map(line => line.split(""));
     const moves = data[1].split("\r\n").join("");
 
+    let mapCopy = structuredClone(map);
+
     let start;
 
     for (let i = 1; i < map.length-1; i++) {
@@ -59,17 +61,110 @@ fs.readFile('input.txt','utf-8',(err,inputData) => {
         }
     }
     
-    // map.forEach(line => console.log(line.join("")));
-
     let part1 = 0;
 
     for (let i = 1; i < map.length-1; i++) {
         for (let j = 1; j < map[0].length-1; j++) {
             if(map[i][j] === "O") {
-                part1 += 100 * i + j
+                part1 += 100 * i + j;
             }
         }
     }
 
     console.log(part1);
+
+    let wideMap = structuredClone(mapCopy);
+
+    for (let i = 0; i < mapCopy.length; i++) {
+        for (let j = 0; j < mapCopy[0].length; j++) {
+            if(mapCopy[i][j] === "#") {
+                wideMap[i][2*j] = "#";
+                wideMap[i][2*j+1] = "#";
+            } else if(mapCopy[i][j] === "O") {
+                wideMap[i][2*j] = "[";
+                wideMap[i][2*j+1] = "]";
+            } else if(mapCopy[i][j] === "@") {
+                start = [i,2*j]
+                wideMap[i][2*j] = "@";
+                wideMap[i][2*j+1] = ".";
+            } else {
+                wideMap[i][2*j] = ".";
+                wideMap[i][2*j+1] = ".";
+            }
+        }
+    }
+
+    [r,c] = start;
+
+    for(let m = 0; m < moves.length; m++) {
+        if(moves[m] === "^") dir = [-1,0];
+        if(moves[m] === ">") dir = [0,1];
+        if(moves[m] === "v") dir = [1,0];
+        if(moves[m] === "<") dir = [0,-1];
+        
+        let [nr,nc] = [r+dir[0],c+dir[1]];
+
+        if(wideMap[nr][nc] === "#") continue;
+        if(wideMap[nr][nc] === ".") {
+            [wideMap[nr][nc], wideMap[r][c]] = [wideMap[r][c], wideMap[nr][nc]];
+            [r,c] = [nr,nc];
+            continue;
+        }
+
+        // if [nr,nc] = "[" or "]":
+        if(wideMap[nr][nc] === "[" || wideMap[nr][nc] === ']') {
+            let shouldMove = true;
+
+            let trail = new Set();
+            if(wideMap[nr][nc] === "[") {
+                trail.add(JSON.stringify([nr,nc,"["]));
+                trail.add(JSON.stringify([nr,nc+1,"]"]));
+            }
+            if(wideMap[nr][nc] === "]") {
+                trail.add(JSON.stringify([nr,nc-1,"["]));
+                trail.add(JSON.stringify([nr,nc,"]"]));
+            }
+            for(const partOfBox of trail) {
+                let [tr,tc] = JSON.parse(partOfBox);
+                if(wideMap[tr+dir[0]][tc+dir[1]] === "#") {
+                    shouldMove = false;
+                    trail.clear();
+                    break;
+                } else if(wideMap[tr+dir[0]][tc+dir[1]] === "[") {
+                    trail.add(JSON.stringify([tr+dir[0],tc+dir[1],"["]));
+                    trail.add(JSON.stringify([tr+dir[0],tc+dir[1]+1,"]"]));
+                } else if(wideMap[tr+dir[0]][tc+dir[1]] === "]") {
+                    trail.add(JSON.stringify([tr+dir[0],tc+dir[1]-1,"["]));
+                    trail.add(JSON.stringify([tr+dir[0],tc+dir[1],"]"]));
+                }
+            }
+
+            if(shouldMove) {
+                for(const box of trail) {
+                    let [br,bc,bs] = JSON.parse(box);
+                    wideMap[br][bc] = '.';
+                    wideMap[br+dir[0]][bc+dir[1]] = bs;
+                }
+                for(const box of trail) {
+                    let [br,bc,bs] = JSON.parse(box);
+                    wideMap[br+dir[0]][bc+dir[1]] = bs;
+                }
+                wideMap[r][c] = ".";
+                wideMap[nr][nc] = "@";
+                [r,c] = [nr,nc];
+                trail.clear();
+            }
+        }
+    }
+
+    let part2 = 0;
+    for (let i = 1; i < wideMap.length-1; i++) {
+        for (let j = 1; j < wideMap[0].length-1; j++) {
+            if(wideMap[i][j] === "[") {
+                part2 += 100 * i + j;
+            }
+        }
+    }
+
+    console.log(part2);
 });
