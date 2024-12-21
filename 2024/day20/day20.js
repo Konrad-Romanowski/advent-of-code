99fa17f0d216cbc8098c516718085213aba0eb5c
Konrad Romanowski <konrad.romanowski.it@gmail.com>
2024-12-21 12:28:37 +0100

    Added day20 part2 solution

--- 2024/day20/day20.js
+++ 2024/day20/day20.js
@@ -8,7 +8,7 @@ fs.readFile('input.txt','utf-8',(err,inputData) => {
 
     const ROWS = map.length;
     const COLS = map[0].length;
-    let start, end;
+    let end;
 
     for (let i = 0; i < map.length; i++) {
         for (let j = 0; j < map.length; j++) {
@@ -60,4 +60,27 @@ fs.readFile('input.txt','utf-8',(err,inputData) => {
     }
 
     console.log(part1);
+
+    let part2 = 0;
+    const maxCheatDuration = 20;
+    for(const pos of path) {
+        let [r,c] = JSON.parse(pos);
+        for(let dr = -maxCheatDuration; dr <= maxCheatDuration; dr++) {
+            for(let dc = -maxCheatDuration; dc <= maxCheatDuration; dc++) {
+                let abs = Math.abs(dr) + Math.abs(dc);
+                if(abs <= maxCheatDuration) {
+                    let nr = r + dr;
+                    let nc = c + dc;
+                    if(
+                        0 <= nr && nr < ROWS && 0 <= c && nc < COLS &&
+                        map[nr][nc]-map[r][c]-abs >= timeToSave
+                    ) {
+                        part2++;
+                    }        
+                }
+            }
+        }
+    }
+
+    console.log(part2);
 });
