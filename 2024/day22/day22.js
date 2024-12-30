bf605fc74a7531a9dd32629a8b426cea5b513f9e
Konrad Romanowski <konrad.romanowski.it@gmail.com>
2024-12-30 16:19:24 +0100

    Added day22 part2 solution

--- 2024/day22/day22.js
+++ 2024/day22/day22.js
@@ -13,13 +13,16 @@ fs.readFile('input.txt','utf-8',(err,inputData) => {
     
     if(err) return console.log(err);
     
-    const prices = inputData.split("\r\n").map(price => parseInt(price)); // change to int
+    const secretNums = inputData.split("\r\n").map(num => parseInt(num));
 
     let finalSecretNums = [];
+    let max = {val: 0, seq: ""};
+    let sequenceSum = {};
 
-    for(const price of prices) {
-        let secretNum = price;
-        for(let i = 0; i < 2000; i++) {
+    for(let secretNum of secretNums) {
+        const seen = new Set();
+        const bananasPrices = [secretNum % 10];
+        for(let i = 1; i <= 2000; i++) {
             // step I
             // Calculate the result of multiplying the secret number by 64.
             // Then, mix this result into the secret number.
@@ -43,10 +46,39 @@ fs.readFile('input.txt','utf-8',(err,inputData) => {
             secretNum = mix(step3result,secretNum); 
             // Finally, prune the secret number.
             secretNum = prune(secretNum);
+
+            const price = secretNum % 10;
+            bananasPrices.push(price);
+
+            if(i >= 4) {
+                const changesSeq = JSON.stringify([
+                    bananasPrices[i-3] - bananasPrices[i-4],
+                    bananasPrices[i-2] - bananasPrices[i-3],
+                    bananasPrices[i-1] - bananasPrices[i-2],
+                    price - bananasPrices[i-1]
+                ]);
+
+                if(!seen.has(changesSeq)) {
+                    seen.add(changesSeq);
+                    if(changesSeq in sequenceSum) {
+                        sequenceSum[changesSeq] += price;
+                    } else {
+                        sequenceSum[changesSeq] = price;
+                    }
+                    
+                    if(max.val < sequenceSum[changesSeq]) {
+                        max.val = sequenceSum[changesSeq];
+                        max.seq = changesSeq;
+                    }
+                }
+            }
         }
-        finalSecretNums.push(secretNum)
+        finalSecretNums.push(secretNum);
     }
 
     let part1 = finalSecretNums.reduce((sum,num) => sum+=num);
     console.log(part1);
+
+    let part2 = max.val;
+    console.log(part2);
 });
