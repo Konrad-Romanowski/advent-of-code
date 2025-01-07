1cc46138a3c8e8750fc8595cad472f9c846dabe5
Konrad Romanowski <konrad.romanowski.it@gmail.com>
2025-01-07 20:22:16 +0100

    Added day09 part2 solution

--- 2024/day09/day09.js
+++ 2024/day09/day09.js
@@ -4,7 +4,7 @@ fs.readFile('input.txt','utf-8',(err,inputData) => {
     
     if(err) return console.log(err);
     
-    const diskSpace = [];
+    const disk = [];
     let id = 0;
 
     for(let i=0; i<inputData.length; i+=2) {
@@ -12,34 +12,82 @@ fs.readFile('input.txt','utf-8',(err,inputData) => {
         let numOfSpaces = parseInt(inputData[i+1]);
 
         for(let j=0; j<numOfBlocks; j++) {
-            diskSpace.push(id);
+            disk.push(id);
         }
         for(let j=0; j<numOfSpaces; j++) {
-            diskSpace.push(".");
+            disk.push(".");
         }
         id++;
     }
 
+    const maxID = id-1;
+    const diskPart1 = [...disk];
     let l = 0;
-    let r = diskSpace.length-1;
+    let r = diskPart1.length-1;
 
     while(l<r) {
-        while(diskSpace[l] !== ".") {
+        while(diskPart1[l] !== ".") {
             l++;
         }
         if(l >= r) break;
-        diskSpace[l] = diskSpace[r];
-        diskSpace[r] = ".";
+        diskPart1[l] = diskPart1[r];
+        diskPart1[r] = ".";
         r--;
     }
 
     let part1 = 0;
     id = 0;
-
-    while(diskSpace[id] !== '.' && id < diskSpace.length) {
-        part1+= id*diskSpace[id];
+    while(diskPart1[id] !== '.' && id < diskPart1.length) {
+        part1+= id*diskPart1[id];
         id++;
     }
-
     console.log(part1);
+
+    
+    const diskPart2 = [...disk];
+
+    let [R1,R2] = [diskPart2.length-1,diskPart2.length-1];
+    for(let id = maxID; id >= 0; id--) {
+        while(diskPart2[R2] !== id) {
+            R2--;
+        }
+        R1 = R2;
+        while(diskPart2[R1] === id) {
+            R1--;
+        }
+        R1++;
+
+        let reqEmptySpace = R2+1-R1;
+        let [L1,L2] = [0,0];
+        let isFreeSpace = false;
+        
+        while(L2 <= R1) {
+            while(diskPart2[L1] !== '.') {
+                L1++;
+            }
+            L2 = L1;
+            while(diskPart2[L2] === '.') {
+                L2++;
+            }
+            if(L2-L1 >= reqEmptySpace) {
+                isFreeSpace = true;
+                break;
+            }
+            L1 = L2;
+        }
+
+        if(isFreeSpace && L2 <= R1) {
+            for(let i = 0; i < reqEmptySpace; i++) {
+                [diskPart2[L1+i],diskPart2[R1+i]] = [diskPart2[R1+i],diskPart2[L1+i]]
+            }
+        }
+    }
+
+    let part2 = 0;
+    for(let i = 0; i < diskPart2.length; i++) {
+        if(typeof diskPart2[i] === 'number') {
+            part2 += i * diskPart2[i];
+        }
+    }
+    console.log(part2);
 });
