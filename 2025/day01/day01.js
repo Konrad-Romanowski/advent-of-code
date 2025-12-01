25c9b1bce1ccd35c4f89dd273845c389f2c0952c
Konrad Romanowski <konrad.romanowski.it@gmail.com>
2025-12-01 18:33:19 +0100

    Added day01 solution - 2025

---
+++ 2025/day01/day01.js
@@ -0,0 +1,28 @@
+import fs from 'fs';
+
+fs.readFile('input.in','utf-8',(err,inputData) => {    
+    if(err) return console.log(err);
+    
+    const rotations = inputData.split("\r\n");
+    let p1 = 0;
+    let p2 = 0;
+    let cur = 50;
+
+    rotations.forEach(instruction => {
+        let dir = instruction[0] === "L" ? -1 : 1;
+        let steps = dir * parseInt(instruction.slice(1));
+        
+        while(steps !== 0) {
+            cur += dir;
+            if(dir === 1 && cur === 100) cur = 0;
+            if(dir === -1 && cur === -1) cur = 99;
+            if(cur === 0) p2++;
+            steps -= dir;
+        }
+
+        if(cur === 0) p1++;
+    });
+
+    console.log(`p1: ${p1}`);
+    console.log(`p2: ${p2}`);
+});
