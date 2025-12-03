767e2142277764cdf2f49212397109e4621914cc
Konrad Romanowski <konrad.romanowski.it@gmail.com>
2025-12-03 20:16:59 +0100

    Added day03 part2 solution - 2025

--- 2025/day03/day03.js
+++ 2025/day03/day03.js
@@ -1,12 +1,18 @@
 import fs from 'fs';
 
+// Shoutout to reddit user Just-Routine-5505,
+// whose visualizations helped me solve part 2 of this problem.
+// https://www.reddit.com/r/adventofcode/comments/1pd0cp6/2025_day_03_cli_visualization/
+
 fs.readFile('input.in','utf-8',(err,inputData) => {    
     if(err) return console.log(err);
 
     const banks = inputData.split('\r\n');
     let p1 = 0;
+    let p2 = 0;
 
     banks.forEach(bank => {
+        // part 1
         const N = bank.length;
         let Lmax = new Array(N).fill(parseInt(bank[0]));
         let Rmax = new Array(N).fill(parseInt(bank[N-1]));
@@ -20,9 +26,30 @@ fs.readFile('input.in','utf-8',(err,inputData) => {
         for(let i = 0; i < N-1; i++) {
             max2 = Math.max(max2,10*Lmax[i]+Rmax[i+1]);
         }
+
+        // part 2
+        let max12 = "";
+        let l = 0;
         
+        for(let r = N-12; r < N; r++) {
+            let curMax = -1;
+            let curMaxIdx = -1;
+            
+            for(let i = l; i <= r; i++) {
+                if(parseInt(bank[i]) > parseInt(curMax)) {
+                    curMax = bank[i];
+                    curMaxIdx = i;
+                }
+            }
+
+            max12 += curMax.toString();
+            l = curMaxIdx+1;
+        }
+
         p1 += max2;
+        p2 += parseInt(max12);
     });
 
     console.log(`p1: ${p1}`);
+    console.log(`p2: ${p2}`);
 });
